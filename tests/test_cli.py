4a07b847051bf367ab07228d865f58291f3b675c
Albert DeFusco <albert.defusco@me.com>
2020-10-22 14:01:56 -0400

    one more cli test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,5 @@
+import pytest
+
 from conda_token import cli
 
 
@@ -6,3 +8,8 @@ def test_token_list(remove_token, capsys):
     captured = capsys.readouterr()
     assert ret == 1
     assert captured.err == 'No tokens have been configured for https://repo.anaconda.cloud/repo/\n'
+
+
+def test_token_set_invalid_channel(remove_token):
+    with pytest.raises(SystemExit):
+        cli.cli(['set', 'secret', '--include-archive-channels', 'nope'])
