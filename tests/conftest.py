935be19d6a0258a10b453545833de30048738e0e
Daniel Holth <dholth@anaconda.com>
2024-06-11 16:40:07 -0400

    parametrize repodata url

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,72 +7,94 @@ from conda_token.repo_config import clean_index, token_remove, token_set
 
 
 def pytest_configure(config):
-    warnings.filterwarnings('always')
+    warnings.filterwarnings("always")
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def reset_channels_alias():
     clean_index()
-    run_command(Commands.CONFIG, '--remove-key', 'channels', use_exception_handler=True)
-    run_command(Commands.CONFIG, '--prepend', 'channels', 'defaults', use_exception_handler=True)
-    run_command(Commands.CONFIG, '--set', 'channel_alias', 'https://conda.anaconda.org', use_exception_handler=True)
+    run_command(Commands.CONFIG, "--remove-key", "channels", use_exception_handler=True)
+    run_command(
+        Commands.CONFIG, "--prepend", "channels", "defaults", use_exception_handler=True
+    )
+    run_command(
+        Commands.CONFIG,
+        "--set",
+        "channel_alias",
+        "https://conda.anaconda.org",
+        use_exception_handler=True,
+    )
 
 
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture(scope="function", autouse=True)
 def set_ssl_verify_true():
-    run_command(Commands.CONFIG, '--set', 'ssl_verify', 'true', use_exception_handler=True)
+    run_command(
+        Commands.CONFIG, "--set", "ssl_verify", "true", use_exception_handler=True
+    )
     yield
-    run_command(Commands.CONFIG, '--set', 'ssl_verify', 'true', use_exception_handler=True)
+    run_command(
+        Commands.CONFIG, "--set", "ssl_verify", "true", use_exception_handler=True
+    )
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def remove_token():
     token_remove()
     yield
     token_remove()
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def set_dummy_token():
     token_remove()
-    token_set('SECRET')
+    token_set("SECRET")
     yield
     token_remove()
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def set_secret_token():
     token_remove()
-    secret_token = os.environ.get('CE_TOKEN', '')
+    secret_token = os.environ.get("CE_TOKEN", "")
     token_set(secret_token)
     yield
     token_remove()
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def set_secret_token_with_signing():
     token_remove()
-    secret_token = os.environ.get('CE_TOKEN', '')
+    secret_token = os.environ.get("CE_TOKEN", "")
     token_set(secret_token, enable_signature_verification=True)
     yield
     token_remove()
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def secret_token():
-    token = os.environ.get('CE_TOKEN', '')
+    token = os.environ.get("CE_TOKEN", "")
     yield token
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def uninstall_rope():
-    run_command(Commands.REMOVE, 'rope', '-y', use_exception_handler=True)
+    run_command(Commands.REMOVE, "rope", "-y", use_exception_handler=True)
     yield
-    run_command(Commands.REMOVE, 'rope', '-y', use_exception_handler=True)
+    run_command(Commands.REMOVE, "rope", "-y", use_exception_handler=True)
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def uninstall_colorama():
-    run_command(Commands.REMOVE, 'colorama', '-y', use_exception_handler=True)
+    run_command(Commands.REMOVE, "colorama", "-y", use_exception_handler=True)
     yield
-    run_command(Commands.REMOVE, 'colorama', '-y', use_exception_handler=True)
+    run_command(Commands.REMOVE, "colorama", "-y", use_exception_handler=True)
+
+
+@pytest.fixture
+def channeldata_url():
+    return "https://repo.anaconda.cloud/repo/main/channeldata.json"
+
+
+@pytest.fixture
+def repodata_url():
+    return "https://repo.anaconda.cloud/repo/main/osx-64/repodata.json"
--- tests/test_token.py
+++ tests/test_token.py
@@ -2,6 +2,7 @@ import pytest
 from conda.cli.python_api import Commands, run_command
 from conda_token.repo_config import CondaTokenError, get_ssl_verify, token_list, validate_token
 from requests import HTTPError
+from urllib.parse import urlparse, urlunparse
 
 try:
     from conda.gateways.connection.session import CondaHttpAuth, CondaSession
@@ -9,25 +10,25 @@ except ImportError:
     from conda.connection import CondaHttpAuth, CondaSession
 
 
-def test_add_token(set_dummy_token):
+def test_add_token(set_dummy_token, repodata_url):
     assert token_list()['https://repo.anaconda.cloud/repo/'] == 'SECRET'
 
-    base_url = 'https://repo.anaconda.cloud/repo/main/osx-64/repodata.json'
-    token_url = 'https://repo.anaconda.cloud/t/SECRET/repo/main/osx-64/repodata.json'
+    base_url = repodata_url # 'https://repo.anaconda.cloud/repo/main/osx-64/repodata.json'
+    scheme, netloc, path, *rest = urlparse(base_url)
+    path = '/t/SECRET' + path
+    token_url = urlunparse((scheme, netloc, path, *rest))
     assert CondaHttpAuth.add_binstar_token(base_url) == token_url
 
 
-def test_channeldata_403(remove_token):
+def test_channeldata_403(remove_token, channeldata_url):
     session = CondaSession()
-    channeldata_url = 'https://repo.anaconda.cloud/repo/main/channeldata.json'
     r = session.get(channeldata_url)
     with pytest.raises(HTTPError):
         r.raise_for_status()
     assert r.status_code == 403
 
 
-def test_repodata_200(set_secret_token):
-    repodata_url = 'https://repo.anaconda.cloud/repo/main/osx-64/repodata.json'
+def test_repodata_200(set_secret_token, repodata_url):
     token_url = CondaHttpAuth.add_binstar_token(repodata_url)
 
     session = CondaSession()
